eeadbac38256f230d05f191edd078dbf08d0eae2
varayoudfederico <varayoudfederico@gmail.com>
2022-05-19 09:00:01 -0300

    Fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { CameraProvider } from "../context/CameraContext";
+import "webrtc-adapter";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
